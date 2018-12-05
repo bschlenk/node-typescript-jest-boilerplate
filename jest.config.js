d3513575e0ddd78768605d19e62a97a2415aa19a
Brian Schlenker <bschlenk@umich.edu>
2018-12-04 20:54:55 -0500

    fix: linting errors & ignore dist for tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+  ],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!**/*.d.ts'
