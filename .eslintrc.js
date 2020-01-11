2defbb9fe9274f39d67e57f92b0d29f11b3ef6ea
Brian Schlenker <bschlenk@umich.edu>
2020-01-11 14:42:13 -0500

    feat: switch to eslint & update config
    Switch away from airbnb config to using eslint recommended & typescript
    eslint recommended. Run eslint --fix on commit.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  root: true,
+
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+
+  env: {
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+
+  rules: {
+    'arrow-body-style': ['error', 'as-needed'],
+    'prefer-arrow-callback': ['error'],
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: false },
+    ],
+  },
+};
